c47156bdb0feb4b00c63fae32805be007bd424ad
cloudfiers <jericmagaway@yahoo.com.ph>
2017-02-23 13:30:16 +0800

    Add a Class Movie

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -9,6 +9,8 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
-
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
     }
 }
